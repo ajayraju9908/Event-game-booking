efa5d036721c906a56f9ae561037f8d503d385d8
ajay <ajayraju9978@gmail.com>
2023-03-06 09:49:57 +0530

    Event game bookig

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Cart from './Components/Cart';
+import CheckoutPage from './Components/Checkoutpage';
+import GameList from './Components/Gamelist';
+import Login from './Components/Login';
+
+import Signup from './Components/Signup';
+
+import { useState } from 'react';
 
 function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
-}
+    const [cartItems, setCartItems] = useState([]);
+    function addToCart(game) {
+        setCartItems([...cartItems, game]);
+        }
+return (
+<Router>
+<Routes>
+<Route path="/" element={<Login />} />
+<Route path="/signup" element={<Signup />} />
+<Route path="/gamelist" element={<GameList cartItems={cartItems} addToCart={addToCart} />} />
+<Route path="/checkoutpage" element={<CheckoutPage/>} />
+<Route path="/cart" element={<Cart cartItems={cartItems}/>} />
 
+</Routes>
+
+</Router>
+);
+}
 export default App;
